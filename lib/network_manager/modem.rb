919f948b4b2ccf07969b00f718d2b83c4fd22dda
Ernest Bursa <ernest@bzdury.pl>
2013-03-30 15:32:31 +0100

    moving forward

---
+++ lib/network_manager/modem.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+class NetworkManager::Modem
+  attr_reader :bus_path
+
+  def initialize(opts)
+    args.each do |k,v|
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end
+  end
+
+  class << self
+    def fetch(paths_array)
+      devices = []
+      paths_array.each do |path|
+        devices << self.new(bus_path: path)
+      end
+      devices
+    end
+  end
+
+end
---
+++ lib/ruby-network-manager.rb
@@ -0,0 +1,36 @@
+# encoding: utf-8
+class NetworkManager
+  attr_reader :service, :bus_path, :mm_object
+
+  def initialize(opts = {})
+    set_options opts
+    @bus        = DBus::SystemBus.instance
+    @mm_service = @bus[@service]
+    @mm_object  = @mm_service.object(@bus_path)
+    @mm_object.introspect
+  end
+
+  # Discover all enabled devices
+  def devices(opts = {})
+    @devices = []
+    @mm_object.introspect
+    @mm_object.EnumerateDevices()
+    
+  end
+
+  class << self
+
+  end
+
+  protected
+
+  def set_options(opts = {})
+    @service    ||= 'org.freedesktop.ModemManager'
+    @bus_path   ||= '/org/freedesktop/ModemManager'
+
+    args.each do |k,v|
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end
+  end
+
+end
