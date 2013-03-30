9592958253e01e793a277f3a7759529ece42934a
Ernest Bursa <ernest@bzdury.pl>
2013-03-30 20:29:20 +0100

    readme fix

--- lib/ruby-network-manager.rb
+++ lib/ruby-network-manager.rb
@@ -2,7 +2,7 @@
 require 'network_manager/modem'
 
 class NetworkManager
-  attr_reader :service, :bus_path, :mm_object
+  attr_reader :modem_service, :modem_bus_path
 
   DBUS_PROPERTIES                     = 'freedesktop.DBus.Properties'
   MM_DBUS_SERVICE                     = 'org.freedesktop.ModemManager'
@@ -16,15 +16,15 @@ class NetworkManager
   def initialize(opts = {})
     set_options opts
     @bus        = DBus::SystemBus.instance
-    @mm_service = @bus[@service]
-    @mm_object  = @mm_service.object(@bus_path)
-    @mm_object.introspect
   end
 
   # Discover all enabled devices
-  def devices(opts = {})
-    @devices = []
+  def modem_devices(opts = {})
+    @mm_service ||= @bus[@modem_service]
+    @mm_object  ||= @mm_service.object(@modem_bus_path)
     @mm_object.introspect
+
+    @devices = []
     NetworkManager::Modem.fetch(@mm_object.EnumerateDevices(), service: @mm_service)
   end
 
@@ -35,8 +35,8 @@ class NetworkManager
   protected
 
   def set_options(opts = {})
-    @service    ||= MM_DBUS_SERVICE || 'org.freedesktop.ModemManager'
-    @bus_path   ||= '/org/freedesktop/ModemManager'
+    @modem_service  ||= MM_DBUS_SERVICE || 'org.freedesktop.ModemManager'
+    @modem_bus_path ||= '/org/freedesktop/ModemManager'
 
     args.each do |k,v|
       instance_variable_set("@#{k}", v) unless v.nil?
